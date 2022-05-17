c484a22d3057269614076217434b3263967451b3
Kim <howtoad123@gmail.com>
2022-05-17 10:29:18 +0200

    123

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,28 @@
 import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [pending, setPending] = useState(true);
+  const {
+    data: blogs,
+    pending,
+    error,
+  } = useFetch(
+    "https://raw.githubusercontent.com/iamshaunjp/Complete-React-Tutorial/lesson-19/dojo-blog/data/db.json"
+  );
   //   const [name, setName] = useState("mario");
 
   //   const handleDelete = (id) => {
   //     const newBlogs = blogs.filter((blog) => blog.id != id);
   //     setBlogs(newBlogs);
   //   };
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(
-        "https://raw.githubusercontent.com/iamshaunjp/Complete-React-Tutorial/lesson-16/dojo-blog/data/db.json"
-      )
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data.blogs);
-          setPending(false);
-        });
-    }, 1000);
-  }, []);
+
   return (
     <div className="home">
+      {error && <div> {error} </div>}
       {pending && <div>loading..</div>}
       {blogs && (
-        <Bloglist blogs={blogs} title="All blogs" /> //handleDelete={handleDelete} />
+        <Bloglist blogs={blogs.blogs} title="All blogs" /> //handleDelete={handleDelete} />
       )}
     </div>
   );
