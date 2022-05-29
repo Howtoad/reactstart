994a270e0426e7db6078884d10d0316d0ad18000
Kim <howtoad123@gmail.com>
2022-05-29 17:25:18 +0200

    its all fixed

--- src/Edit.js
+++ src/Edit.js
@@ -12,9 +12,9 @@ const Edit = () => {
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const blog = { title, body, author };
+
     setPending(true);
-    fetch("http://localhost:8000/blogs/" + { id }, {
+    fetch("http://localhost:8000/blogs/" + id, {
       method: "PATCH",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(blog),
@@ -25,33 +25,48 @@ const Edit = () => {
   };
   return (
     <div className="create">
+      {blog && (
+        <>
+          <label>
+            Blog Title:
+            <input
+              type="text"
+              required
+              value={blog.title}
+              onChange={(e) => {
+                blog.title = e.target.value;
+                setTitle(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Blog body:
+            <textarea
+              required
+              value={blog.body}
+              onChange={(e) => {
+                blog.body = e.target.value;
+                setBody(e.target.value);
+              }}
+            ></textarea>
+          </label>
+
+          <label>Blog author:</label>
+          <select
+            value={blog.author}
+            onChange={(e) => {
+              blog.author = e.target.value;
+              setAuthor(e.target.value);
+            }}
+          >
+            <option value="mario">Mario</option>
+            <option value="yoshi">Yoshi</option>
+          </select>
+        </>
+      )}
       <h2>Add new blog</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Blog Title:
-          <input
-            type="text"
-            required
-            value={blog.title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-
-        <label>
-          Blog body:
-          <textarea
-            required
-            value={blog.body}
-            onChange={(e) => setBody(e.target.value)}
-          ></textarea>
-        </label>
-
-        <label>Blog author:</label>
-        <select value={blog.author} onChange={(e) => setAuthor(e.value.target)}>
-          <option value="mario">Mario</option>
-          <option value="yoshi">Yoshi</option>
-        </select>
-
         {!pending && <button type="submit">Edit the blog</button>}
         {pending && <button disabled>Editing blog..</button>}
       </form>
